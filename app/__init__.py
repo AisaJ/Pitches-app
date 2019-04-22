644e890911caa431f965cddc383fdcbe7fd43f53
Aisa <aisajemila@gmail.com>
2019-04-22 10:04:17 +0300

    Initialied blueprint in the main and registered in the app.

---
+++ app/__init__.py
@@ -0,0 +1,10 @@
+from flask import Flask
+
+def creat_app(config_name):
+  app = Flask(__name__)
+
+  #Registering blueprint
+  from main import main as main_blueprint
+  app.register_blueprint(main_blueprint)
+
+  return app
