a6eb4c600d7834aa33cebfc65667bae73075c905
Aisa <aisajemila@gmail.com>
2019-04-22 18:40:42 +0300

    configured the development server to setup the database and application.

--- manage.py
+++ manage.py
@@ -1,2 +1,19 @@
 from app import create_app,db
 from app.models import Pitch
+from flask_script import Server,Manager
+
+#Creating an app instance
+app = create_app('production')
+
+manager = Manager(app)
+manager.add_command('server',Server)
+@manager.command
+def test():
+    """Run the unit tests."""
+    import unittest
+    tests = unittest.TestLoader().discover('tests')
+    unittest.TextTestRunner(verbosity=2).run(tests)
+
+
+if __name__ == '__main__':
+  manager.run()
