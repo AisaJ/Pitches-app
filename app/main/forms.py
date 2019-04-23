270e82cb8f63496fca1d9ee2ed51d95a40f3a6d8
Aisa <aisajemila@gmail.com>
2019-04-23 14:08:20 +0300

    Created Pitch form.

--- app/main/forms.py
+++ app/main/forms.py
@@ -2,6 +2,10 @@ from flask_wtf import FlaskForm
 from wtforms import SelectField,TextAreaField,SubmitField,StringField
 from wtforms.validators import required
 
-class CategoryForm(FlaskForm):
+class PitchForm(FlaskForm):
 
-  category = SelectField('category_id')
+  title = StringField('Pitch title',validators=[Required()])
+  category = SelectField("Choose Category",choices=[('Personal Brand','personal brand'),('Product','product'),('Project','project'),('Investor','investor')])
+  review = TextAreaField('Your Pitch')
+  name = StringField('Preferred name')
+  submit = SubmitField('Submit')
