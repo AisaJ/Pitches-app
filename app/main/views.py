5ddeb28782bcc5b2fb29a87a45ca28853acbb866
Aisa <aisajemila@gmail.com>
2019-04-23 19:26:22 +0300

    Added load user function by passing in a user_id to the function that queeries the database to get user with the id.

--- app/main/views.py
+++ app/main/views.py
@@ -8,5 +8,22 @@ from flask_login import login_required
 def index():
 
   title = 'What can 30 seconds do for you?'
+  pitches = Pitch.query.all()
 
   return render_template('index.html',title = title)
+
+@main.route('/pitch/new',methods = ['GET','POST'])
+@login_required
+def new_pitch():
+  form = PitchForm()
+
+  if form.validate_on_submit():
+    title=form.title.data
+
+    db.session.add(pitch)
+    db.session.commit()
+
+    flash('Awesome! Pitch created','success')
+    return redirect(url_for('main.index',id=pitch.id))
+
+  return render_template('new_pitch.html',title='new pitch',pitch_form=form, post='new pitch')
--- app/models.py
+++ app/models.py
@@ -4,6 +4,11 @@ from werkzueg.security import generate_password_hash,check_password_hash
 from flask_login import UserMixin
 from . import login_manager
 
+@login_manager.user_loader
+def load_user(user_id):
+    return User.query.get(int(user_id))
+
+
 class Pitch(db.Model):
   '''
   Class to define pitch objects
@@ -21,7 +26,7 @@ class Pitch(db.Model):
     return f'Pitch{self.pitch}'
 
 
-class Users(UserMixin,db.Model):
+class User(UserMixin,db.Model):
   '''
   Defining users objects
   '''
