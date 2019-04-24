a18ed0dbe1a908d29b83fa4682abc8e0c3eb5564
Aisa <aisajemila@gmail.com>
2019-04-24 08:03:37 +0300

    Created instance of Login form that passes to the login html.

--- app/auth/views.py
+++ app/auth/views.py
@@ -1,9 +1,19 @@
 from flask import render_template
 from . import auth
 
-@auth.route('/login')
+@auth.route('/login',methods = ['GET','POST'])
 def login():
-  return render_template('auth/login.html')
+  login_form = LoginForm()
+  if login_form.validate_on_submit():
+        user = User.query.filter_by(email = login_form.email.data).first()
+        if user is not None and user.verify_password(login_form.password.data):
+            login_user(user,login_form.remember.data)
+            return redirect(request.args.get('next') or url_for('main.index'))
+
+        flash('Invalid username or Password')
+
+    title = "Pitch login"
+  return render_template('auth/login.html',title=title,login_form=login_form)
 
 @auth.route('/register',methods=['GET','POST'])
 def register():
