2f3ac0852fd62c77b36b0ec23ab9f9e854d624cf
Aisa <aisajemila@gmail.com>
2019-04-24 10:19:50 +0300

    Added profile view function.

--- app/main/views.py
+++ app/main/views.py
@@ -1,6 +1,6 @@
 from . import main
-from flask import render_template
-from ..models import Pitch
+from flask import render_template,request,redirect,url_for,abort
+from ..models import Pitch,User
 from flask_login import login_required
 
 
@@ -30,3 +30,12 @@ def new_pitch():
     return redirect(url_for('main.index',id=pitch.id))
 
   return render_template('pitch.html',title='new pitch',pitch_form=form, post='new pitch')
+
+@main.route('/user/<uname>')
+def profile(uname):
+  user = User.query.filter_by(username=uname).first()
+
+  if user is None:
+    abort(404)
+
+  return render_template('profile/profile.html',user=user)
